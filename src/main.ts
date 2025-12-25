8a6e1e6853aeb590730add568d6d1ba6bce0c830
Kevin Lint <kevin@concreit.com>
2025-12-25 12:32:04 -0800

    setup

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { UI } from "@/app";
-import { fsm } from "@/fsm";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const root = document.getElementById("root");
-  if (!root) return console.error("Root element not found");
-  root.append(UI);
-  fsm("INIT");
-});
